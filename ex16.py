ea330d637061112fce4070370bb09bb36271584d
kofi-teddy <teddyagud@teddys-MacBook-Pro.local>
2021-09-03 08:02:48 +0000

    String formating

---
+++ ex16.py
@@ -0,0 +1,9 @@
+# Formating strings
+
+first_name = "Teddy"
+last_name = "Agudogo"
+
+# output = "Hello, {} {}".format(first_name, last_name)
+output = f"Hello {first_name} {last_name}"
+
+print(output)
