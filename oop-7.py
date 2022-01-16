2fe7fda3505976b9e693fe680628bd143bd860dd
kofi-teddy <agudogot@gmail.com>
2022-01-16 19:03:10 +0000

    multiple inheritance

--- oop-7.py
+++ oop-7.py
@@ -5,6 +5,16 @@ from __future__ import annotations
 from pprint import pprint
 
 
+class Emailable():
+    email: str
+
+
+class MailSender(Emailable):
+    def send_mail(self, message: str) -> None:
+        print(f'Sending mail to {self.email=}')  
+        # add email logic here
+
+
 class ContactList(list['Contact']):
     def search(self, name: str) -> list['Contact']:
 
@@ -45,6 +55,11 @@ class Friend(Contact):
         super().__init__(name, email)
         self.phone = phone
 
+
+class EmailableContact(Contact, MailSender):
+    pass
+
+
 # c_1 = Contact('kofi', 'kofi@skycrew.tech')
 # s_1 = Supplier('louis', 'louis@skycrew.tech')
 # print(c_1.name, c_1.email, s_1.name, s_1.email)
@@ -57,13 +72,18 @@ class Friend(Contact):
 
 # print(Contact.all_contacts)
 
-c1 = Contact('Kofi T', 'kofi@skycrew.tech')
-c1 = Contact('Kofi A', 'Fii@skycrew.tech')
-c2 = Contact('Louis A', 'lous@skycrew.tech')
-c3 = Contact('Sefaah O', 'sefaah@skycrew.tech')
-# print([c.name for c in Contact.all_contacts.search('Kofi')])
+# c1 = Contact('Kofi T', 'kofi@skycrew.tech')
+# c1 = Contact('Kofi A', 'Fii@skycrew.tech')
+# c2 = Contact('Louis A', 'lous@skycrew.tech')
+# c3 = Contact('Sefaah O', 'sefaah@skycrew.tech')
+# # print([c.name for c in Contact.all_contacts.search('Kofi')])
 
-# print([] == list())
+# # print([] == list())
 
-f = Friend('Selorm A', 'selorm@skycrew.tech', '023000000')
+# f = Friend('Selorm A', 'selorm@skycrew.tech', '023000000')
+# print(Contact.all_contacts)
+
+e = EmailableContact('Kofi T', 'kofi@skycrew.tech')
 print(Contact.all_contacts)
+
+print(e.send_mail('Hello, test e-mail here'))
