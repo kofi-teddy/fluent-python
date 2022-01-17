b733f6da48cc5d4048726c6a3695e4198a5205de
kofi-teddy <agudogot@gmail.com>
2022-01-17 09:41:24 +0000

    solving the diamond inheritance problem with super()

--- oop-9.py
+++ oop-9.py
@@ -1,12 +1,77 @@
 # oop
 # extending built-ins 
 
+# class ContactList(list['Contact']):
+#     def search(self, name: str) -> list['Contact']:
 
+#         matching_contacts: list['Contact'] = []
+#         for contact in self:
+#             if name in contact.name:
+#                 matching_contacts.append(contact)
+#         return matching_contacts
+
+
+# class Contact:
+#     # all_contacts: list['Contact'] = []
+#     all_contacts = ContactList()
+
+#     def __init__(self, name: str, email: str) -> None:
+#         self.name = name
+#         self.email = email
+#         Contact.all_contacts.append(self)
+
+#     def __repr__(self) -> str:
+#         return(
+#             f'{self.__class__.__name__}('
+#             f'{self.name!r}, {self.email!r}'
+#             f')'
+#         )
+
+
+# class AddressHolder:
+#     def __init__(self, street: str, city: str, state: str, code: str) -> None:
+#         self.street = street
+#         self.city = city 
+#         self.state = state
+#         self.code = code
+
+
+# class Friend(Contact, AddressHolder):
+#     def __init__(
+#         self,
+#         name: str, 
+#         email: str, 
+#         phone: str, 
+#         street: str, 
+#         city: str, 
+#         state: str, 
+#         code: str) -> None:
+#         Contact.__init__(self, name, email)
+#         AddressHolder.__init__(self, street, city, state, code)
+#         self.phone = phone
+
+
+class ContactList(list['Contact']):
+    def search(self, name: str) -> list['Contact']:
+
+        matching_contacts: list['Contact'] = []
+        for contact in self:
+            if name in contact.name:
+                matching_contacts.append(contact)
+        return matching_contacts
+
+
+# solving the above diamond inheritance problem
 class Contact:
     # all_contacts: list['Contact'] = []
     all_contacts = ContactList()
 
-    def __init__(self, name: str, email: str) -> None:
+    def __init__(
+        self, 
+        /,
+        name: str = '' , 
+        email: str = '',
+        **kwargs: Any) -> None:
         self.name = name
         self.email = email
         Contact.all_contacts.append(self)
@@ -20,7 +85,14 @@ class Contact:
 
 
 class AddressHolder:
-    def __init__(self, street: str, city: str, state: str, code: str) -> None:
+    def __init__(
+        self, 
+        /,
+        street: str = '', 
+        city: str = '', 
+        state: str = '', 
+        code: str = '') -> None:
+        super().__init__(**kwargs) # type: ignore [call-arg]
         self.street = street
         self.city = city 
         self.state = state
@@ -30,13 +102,8 @@ class AddressHolder:
 class Friend(Contact, AddressHolder):
     def __init__(
         self,
-        name: str, 
-        email: str, 
-        phone: str, 
-        street: str, 
-        city: str, 
-        state: str, 
-        code: str) -> None:
-        Contact.__init__(self, name, email)
-        AddressHolder.__init__(self, street, city, state, code)
+        /,
+        phone: str='', 
+        **kwargs: Any) -> None:
+        super().__init__(**kwargs)
         self.phone = phone
