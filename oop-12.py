4dd208ec82ecb9cc5684f647f7c0b3373da93aeb
kofi-teddy <agudogot@gmail.com>
2022-01-18 13:16:59 +0000

    exception handing

---
+++ oop-12.py
@@ -0,0 +1,20 @@
+# oop
+# Exceptions handling  
+
+from typing import List
+
+
+class EvenOnly(List[int]):
+    def append(self, value: int) -> None:
+        if not isinstance(value, int):
+            raise TypeError('Only integers can be added')
+        if value % 2 != 0:
+            raise ValueError('Only even numbers can be added')
+        super().append(value)
+            
+
+# driver code 
+e = EvenOnly()
+# e.append('a string')
+# e.append(3)
+e.append(2)
