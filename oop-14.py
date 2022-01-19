1af6c1e7babc940850a66afe0a6a8e652b388cd7
kofi-teddy <agudogot@gmail.com>
2022-01-19 15:03:33 +0000

    changing classes to accept multiples points

--- oop-14.py
+++ oop-14.py
@@ -57,12 +57,28 @@ class Polygon:
         return sum(p1.distance(p2) for p1, p2 in pairs)
 
 
+class Polygon_2:
+    def __init__(self, vertices: Optional[Iterable[Point]] = None) -> None:
+        self.vertices = list(vertices) if vertices else []
+    
+    def perimeter(self) -> float:
+        pairs = zip(
+            self.vertices, self.vertices[1:] + self.vertices[:1])
+        return sum(p1.distance(p2) for p1, p2 in pairs)
+
+
 # driver code
 # print(perimeter(square))
-square = Polygon()
-square.add_point(Point(1, 1))
-square.add_point(Point(1, 2))
-square.add_point(Point(2, 2))
-square.add_point(Point(2, 1))
+# square = Polygon()
+# square.add_point(Point(1, 1))
+# square.add_point(Point(1, 2))
+# square.add_point(Point(2, 2))
+# square.add_point(Point(2, 1))
+# print(square.perimeter())
+
+square = Polygon_2(
+    [Point(1,1), Point(1,2), Point(2,2), Point(2,1)]
+)
+
 print(square.perimeter())
 
