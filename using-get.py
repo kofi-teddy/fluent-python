388bf8365f4ea0401e5b7bc9f97ce728f7b34927
kofi-teddy <agudogot@gmail.com>
2022-01-23 23:36:50 +0000

    using get() to access dictionaries

---
+++ using-get.py
@@ -0,0 +1,12 @@
+# using .get() to avoid KeyError when accessing
+# non-existing keys inside a dictionary.
+# if no default value it returns None if key does not exist.
+
+
+user = {'name': 'Akosua', 'surname': 'layanda'}
+
+# print(user.get('address', 'Best streets 42'))
+print(user.get('address'))
+
+# KeyError: 'address'
+# print(user['address'])
