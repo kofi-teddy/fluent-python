cb6334d4d9c8a0bd3b9cee9afae50ef0648e8256
kofi-teddy <agudogot@gmail.com>
2022-02-03 10:50:12 +0000

    union intersections and symmetric difference

--- oop-30.py
+++ oop-30.py
@@ -11,13 +11,37 @@ song_library = [
     ("Mal's Song", "Vixy and Tony"),
 ]
 
-# driver code 
-artists = set()
-for song, artist in song_library:
-    artists.add(artist)
+dusty_artists = {
+    'Sarah Brightman',
+    'Guns N" Roses',
+    'Oppeth',
+    'Vixy and Tony',
+}
 
-print(artists)
-print('Opeth' in artists)
-alpabetical = list(artists)
-alpabetical.sort()
-print(alpabetical)
+steve_artists = {'Yes', 'Guns N', 'Genesis'}
+
+artists = {'Gun N Roses', 'Vixy and Tony', 'Sarah Brightman', 'Opeth'}
+bands = {'Opeth', 'Guns N Roses'}
+
+
+# driver code 
+# artists = set()
+# for song, artist in song_library:
+#     artists.add(artist)
+
+# print(artists)
+# print('Opeth' in artists)
+# alpabetical = list(artists)
+# alpabetical.sort()
+# print(alpabetical)
+
+# print(f'All: {dusty_artists | steve_artists}')
+# print(f'Both: {dusty_artists.intersection(steve_artists)}')
+# print(f'Either but not both: {dusty_artists ^ steve_artists}')
+
+print(artists.issuperset(bands))
+print(artists.issubset(bands))
+print(artists - bands) 
+print(bands.issuperset(artists))
+print(bands.issubset(artists))
+print(bands.difference(artists))
