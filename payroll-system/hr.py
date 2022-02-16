97636f3ed627c73465afdcc33d9629994599541f
kofi-teddy <agudogot@gmail.com>
2022-02-16 23:39:06 +0000

    python payroll system hr

---
+++ payroll-system/hr.py
@@ -0,0 +1,10 @@
+#  hr
+
+class PayrollSystem:
+    def calculate_payroll(self, employees):
+        print('Calculating Payroll')
+        print('___________________')
+        for employee in employees:
+            print(f'Payroll for: {employee.id} - {employee.name}')
+            print(f'- Check amount: {employee.calculate_payroll()}')
+            print('')
