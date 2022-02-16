7b0c637f4008e51c52a01bb0c1274547f903787a
kofi-teddy <agudogot@gmail.com>
2022-02-16 23:57:08 +0000

    commissionemployee class

--- payroll-system/hr.py
+++ payroll-system/hr.py
@@ -46,5 +46,13 @@ class HourlyEmployee(Employee):
         return self.hours_worked * self.hour_rate
 
 
+class CommissionEmployee(SalaryEmployee):
+    def __init__(self, id, name, weekly_salary, commission):
+        super().__init__(id, name, weekly_salary)
+        self.commission = commission
+    
+    def calculate_payroll(self):
+        fixed = super().calculate_payroll()
+        return fixed + self.commission
 
     
