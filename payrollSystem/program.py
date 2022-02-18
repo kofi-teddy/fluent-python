b72e58e022547bc9216a6abddb9a0cee97be5c5e
kofi-teddy <agudogot@gmail.com>
2022-02-18 12:43:15 +0000

    modified hr to employee

--- payrollSystem/program.py
+++ payrollSystem/program.py
@@ -1,11 +1,11 @@
 # OOP
 # Driver code
-from . import hr
+from . import employee
 
-salary_employee = hr.SalaryEmployee(1, 'Maxine Baddoo', 1500)
-hourly_employee = hr.HourlyEmployee(2, 'Eunice Boakye', 40, 15)
-commission_employee = hr.CommissionEmployee(3, 'Baaba Aggrey', 1000, 250)
-payroll_system = hr.PayrollSystem()
+salary_employee = employee.SalaryEmployee(1, 'Maxine Baddoo', 1500)
+hourly_employee = employee.HourlyEmployee(2, 'Eunice Boakye', 40, 15)
+commission_employee = employee.CommissionEmployee(3, 'Baaba Aggrey', 1000, 250)
+payroll_system = employee.PayrollSystem()
 payroll_system.calculate_payroll([
     salary_employee,
     hourly_employee,
