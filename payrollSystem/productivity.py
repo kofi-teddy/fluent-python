4ac6a0e26363d298d858f05b399d4595b982d546
kofi-teddy <agudogot@gmail.com>
2022-02-19 16:04:28 +0000

    convert work to performduties

--- payrollSystem/productivity.py
+++ payrollSystem/productivity.py
@@ -1,6 +1,20 @@
 # Productivity app
 
 class ProductivitySystem:
+    def __init__(self):
+        self._roles = {
+            'manager': ManagerRole,
+            'secretary': SecretaryRole,
+            'sales': SalesRole,
+            'factory': FactoryRole,
+        }
+    
+    def get_role(self,role_id):
+        role_type = self._roles.get(role_id)
+        if not role_type:
+            raise ValueError('role_id')
+        return role_type()
+
     def track(self, employees, hours):
         print('Tracking Employee Productivity')
         print('______________________________')
@@ -10,21 +24,21 @@ class ProductivitySystem:
 
 
 class ManagerRole:
-    def work(self, hours):
+    def perform_duties(self, hours):
         print(f'{self.name} screams and yells for {hours} hours.')
 
 
 class SecretaryRole:
-    def work(self, hours):
+    def perform_duties(self, hours):
         print(f'{self.name} expends {hours} hours doing office paperwork.')
 
 
 class SalesRole:
-    def work(self, hours):
+    def perform_duties(self, hours):
         print(f'{self.name} expends {hours} on the phone.')
 
 
 class FactoryRole:
-    def work(self, hours):
+    def perform_duties(self, hours):
         print(f'{self.name} manufactures gadgets for {hours} hours.')
 
