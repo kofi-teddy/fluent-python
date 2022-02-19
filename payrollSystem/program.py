d005bea3f3a60ba88271b884fa1ea02c9afbcd9b
kofi-teddy <agudogot@gmail.com>
2022-02-19 13:08:35 +0000

    updated with temporary secretary driver code

--- payrollSystem/program.py
+++ payrollSystem/program.py
@@ -8,11 +8,13 @@ salary_employee = employee.SalaryEmployee(1, 'Maxine Baddoo', 1500)
 hourly_employee = employee.HourlyEmployee(2, 'Eunice Boakye', 40, 15)
 commission_employee = employee.CommissionEmployee(3, 'Baaba Aggrey', 1000, 250)
 factory_worker = employee.FactoryWorker(5, 'Jamila Brown', 40, 15)
+temporary_secretary = employee.TemporarySecretary(6, 'Loretta Aggrey', 40, 9)
 payroll_system = employee.PayrollSystem()
 payroll_system.calculate_payroll([
     salary_employee,
     hourly_employee,
     commission_employee,
+    temporary_secretary,
 ])
 
 
