7baf403c26088456c616f322bdfa3b13055b2ec4
kofi-teddy <agudogot@gmail.com>
2022-02-19 14:22:22 +0000

    sales upgrade with  docs

--- payrollSystem/employees.py
+++ payrollSystem/employees.py
@@ -52,6 +52,30 @@ class Secretary(Employee, SecretaryRole, SalaryPolicy):
 
 
 class SalesPerson(Employee, SalesRole, CommissionPolicy):
+    '''
+    Sales reps paid a fixed salary plus commission on sales.
+    '''
     def __init__(self, id, name, weekly_salary, commission):
         CommissionPolicy.__init__(self, weekly_salary, commission)
         super().__init__(id, name)
+
+class CommissionEmployee(SalaryEmployee):
+    
+    def __init__(self, id, name, weekly_salary, commission):
+        super().__init__(id, name, weekly_salary)
+        self.commission = commission
+    
+    def calculate_payroll(self):
+        fixed = super().calculate_payroll()
+        return fixed + self.commission
+
+
+class TemporarySecretary(Secretary, HourlyEmployee):
+    '''
+    Temporary workers.
+    '''
+    def __init__(self, id, name, hours_worked, hour_rate):
+        HourlyEmployee.__init__(self, id, name, hours_worked, hour_rate)
+
+    def calculate_payroll(self):
+        return HourlyEmployee.calculate_payroll(self)
