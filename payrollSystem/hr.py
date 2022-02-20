004f817e838c479954a1162f183e7de2ac3b2dc8
kofi-teddy <agudogot@gmail.com>
2022-02-20 15:49:42 +0000

    self._employee_policies fix

--- payrollSystem/hr.py
+++ payrollSystem/hr.py
@@ -3,12 +3,12 @@
 
 class PayrollSystem:
     def __init__(self):
-        self_employee_policies = {
+        self._employee_policies = {
             1: SalaryPolicy(3000),
             2: SalaryPolicy(1500),
             3: CommissionPolicy(1000, 100),
             4: HourlyPolicy(15),
-            5: HourlyPolicy(9)
+            5: HourlyPolicy(9),
         }
 
     def get_policy(self, employee_id):
