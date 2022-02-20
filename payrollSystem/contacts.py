5096317e4b9f75d8d42f642ddb7bc17ce958ede6
kofi-teddy <agudogot@gmail.com>
2022-02-20 14:49:48 +0000

    added addressbook class with init and getemployeeaddress methods

--- payrollSystem/contacts.py
+++ payrollSystem/contacts.py
@@ -15,3 +15,20 @@ class Address:
             lines.append(self.street2)
         lines.append(f'{self.city}, {self.state} {self.zipcode}')
         return '\n'.join(lines)
+
+
+class AddressBook:
+    def __init__(self):
+        self._employee_addresses = {
+            1: Address('Comm. 2 Alang', 'Aggrey road', 'Tema', '2244'),
+            2: Address('Comm. 10', 'May flour Area', 'C10', '2244'),
+            3: Address('East Legon', 'Ahodwo Area', 'Accra', '2244'),
+            4: Address('Lebanon', 'Ashaiman', 'Tema', '2244'),
+            5: Address('Comm. 7', 'Biggies Area', 'Tema', '2244'),
+        }
+    
+    def get_employee_address(self, employee_id):
+        address = self._employee_addresses.get(employee_id)
+        if not address:
+            raise ValueError(employee_id)
+        return  address
