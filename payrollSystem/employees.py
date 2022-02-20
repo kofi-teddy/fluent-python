47db41b4429af5c790a2162732c45d72fa3c598e
kofi-teddy <agudogot@gmail.com>
2022-02-20 15:16:19 +0000

    employeeclass upgrades with work and calculatepayroll methods

--- payrollSystem/employees.py
+++ payrollSystem/employees.py
@@ -11,7 +11,6 @@ from .productivity import (
     SecretaryRole,
 )
 
-
 # class PayrollSystem:
 #     def calculate_payroll(self, employees):
 #         print('Calculating Payroll')
@@ -20,16 +19,6 @@ from .productivity import (
 #             print(f'Payroll for: {employee.id} - {employee.name}')
 #             print(f'- Check amount: {employee.calculate_payroll()}')
 #             print('')
-
-
-class Employee:
-    '''
-    Employee base class for modeling all employees types.
-    '''
-    def __init__(self, id, name):
-        self.id = id
-        self.name = name
-        self.address = None
 
 
 class EmployeeDatabase:
@@ -80,6 +69,30 @@ class EmployeeDatabase:
         employee_role = self.productivity.get_role(role)
         payroll_policy = self.payroll.get_policy(id)
         return Employee(id, name, address, employee_role, payroll_policy)
+
+
+class Employee:
+    '''
+    Employee base class for modeling all employees types.
+    '''
+    def __init__(self, id, name, address, role, payroll):
+        self.id = id
+        self.name = name
+        self.address = None
+        self.address = address
+        self.role = role
+        self.payroll = payroll
+
+    def work(self, hours):
+        duties = self.role.perform_duties(hours)
+        print(f'Employee {self.id} - {self.name}:')
+        print(f'- {duties}')
+        print('')
+        self.payroll.track_work(hours)
+    
+    def calculate_payroll(self):
+        return self.payroll.calculate_payroll()
+
 
 
 class Manager(Employee, ManagerRole, SalaryPolicy):
