8d53690319093ad89e824113b432ac1f62e7579f
kofi-teddy <agudogot@gmail.com>
2022-03-09 15:16:33 +0000

    multiple inheritance

--- oop-34.py
+++ oop-34.py
@@ -16,3 +16,22 @@ class Rectangle:
 class Square(Rectangle):
     def __init__(self, length):
         super().__init__(length, length) 
+
+
+class Cube(Square):
+    def surface_area(self):
+        face_area = super().area()
+        return face_area * 6
+
+    def volume(self):
+        face_area = super().area()
+        return face_area * self.length
+
+
+# Driver code 
+# square = Square(4)
+# print(square.area())
+
+cube = Cube(3)
+print(cube.surface_area())
+print(cube.volume())
