cccd594ffd75326b5cc5b9c94ff6dde835498e72
kofi-teddy <agudogot@gmail.com>
2022-05-02 12:34:11 +0000

    accessing nested list python

---
+++ accesing-nested-list.py
@@ -0,0 +1,38 @@
+user = [
+    {
+        "id": "acf4ffc2-6144-410c-8fa9-a1478c96bdbf",
+        "account": {
+            "id": "59a6eed2-387b-444f-ba1c-50ae9c22b0a2",
+            "wallet": [
+                {
+                    "id": "a33ebb1f-c3b3-4236-8141-05e71c032a50",
+                    "source_transaction": [],
+                    "destination_transaction": [],
+                    "phone": null,
+                    "main_balance_currency": "GHS",
+                    "main_balance": "0.00",
+                    "available_balance_currency": "GHS",
+                    "available_balance": "20000.00",
+                    "category": "personal",
+                    "description": "My Pocket Wallet",
+                    "created_at": "2022-05-01T20:34:00.968243Z",
+                    "update_at": "2022-05-01T20:34:00.968256Z",
+                    "account": "59a6eed2-387b-444f-ba1c-50ae9c22b0a2"
+                }
+            ],
+            "type": "individual",
+            "status": "active",
+            "kyc": null,
+            "created_at": "2022-05-01T20:31:36.695299Z",
+            "update_at": "2022-05-01T20:31:56.877661Z",
+            "user": "acf4ffc2-6144-410c-8fa9-a1478c96bdbf",
+            "created_by": "99b03e54-8255-45b3-8172-fa1bb93873c0",
+            "updated_by": "99b03e54-8255-45b3-8172-fa1bb93873c0"
+        },
+        "phone": "112233445566",
+        "firstname": "Ama",
+        "lastname": "Benyiwaa"
+    }
+]
+
+[0]['account']['wallet'][0]['source_transaction']
