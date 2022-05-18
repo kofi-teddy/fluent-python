d3bea4b7b7f5c144d8307e83211e092981c83162
kofi-teddy <agudogot@gmail.com>
2022-05-18 22:57:02 +0000

    ppprint

--- try-phonefield.py
+++ try-phonefield.py
@@ -1,1 +1,10 @@
-import phonenumber
+# import phonenumber
+
+recipient_phone = "+233208556743"
+sender_phone = "+233208556743"
+recipient_name = "Selorm Agudogo"
+sender_name = "Felix Agudogo"
+amount = 100
+
+message = f"Winpay to Wingpay transfer of GHS{amount} from {sender_phone} - {sender_name} to {recipient_phone} - {recipient_name}"
+print(f"*** {message} ***")
