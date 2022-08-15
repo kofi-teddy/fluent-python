cb812c37bb47605742b2b1860b76d2ed9cb57996
kofi-teddy <agudogot@gmail.com>
2022-08-15 20:55:28 +0000

    clean up

--- playground.py
+++ playground.py
@@ -104,84 +104,84 @@
 
 # print(data["msg"]["sendingHse"]["result"]["system_code"])
 
-data = [
-          {
-              "sex": "Male",
-              "email": "pmarks1914@gmail.com",
-              "balance": -960,
-              "program": "DBS STATISTICS",
-              "currency": "GHS",
-              "fee_type": 1,
-              "mobileNo": "0206358827",
-              "accountNo": "01190001N",
-              "full_name": "DADZIE DERRICK ",
-              "last_name": "",
-              "studentId": "01190001N",
-              "first_name": "",
-              "sessionName": "Morning",
-              "admission_id": "01190001N",
-              "currentLevel": "NON-HND I",
-              "mobile_number": "0543418718",
-              "recipient_address": "0543418718"
-          },
-          {
-              "sex": "Male",
-              "email": "zee@gmail.com",
-              "balance": -960,
-              "program": "DBS STATISTICS",
-              "currency": "GHS",
-              "fee_type": 1,
-              "mobileNo": "0206358827",
-              "accountNo": "01190001N",
-              "full_name": "DADZIE DERRICK ",
-              "last_name": "",
-              "studentId": "01190001N",
-              "first_name": "",
-              "sessionName": "Morning",
-              "admission_id": "01190001N",
-              "currentLevel": "NON-HND I",
-              "mobile_number": "0543418718",
-              "recipient_address": "0543418718"
-          },
-          {
-              "sex": "Male",
-              "email": "zee@gmail.com",
-              "balance": -960,
-              "program": "DBS STATISTICS 000",
-              "currency": "GHS",
-              "fee_type": 1,
-              "mobileNo": "0206358827",
-              "accountNo": "01190001N",
-              "full_name": "DADZIE DERRICK 000",
-              "last_name": "",
-              "studentId": "01190001N",
-              "first_name": "",
-              "sessionName": "Morning",
-              "admission_id": "01190001N",
-              "currentLevel": "NON-HND I",
-              "mobile_number": "0543418718",
-              "recipient_address": "0543418718"
-          },
-          {
-              "sex": "Male",
-              "email": "jay@gmail.com",
-              "balance": -960,
-              "program": "DBS STATISTICS",
-              "currency": "GHS",
-              "fee_type": 1,
-              "mobileNo": "0206358827",
-              "accountNo": "01190001N",
-              "full_name": "DADZIE DERRICK ",
-              "last_name": "",
-              "studentId": "01190001N",
-              "first_name": "",
-              "sessionName": "Morning",
-              "admission_id": "01190001N",
-              "currentLevel": "NON-HND I",
-              "mobile_number": "0543418718",
-              "recipient_address": "0543418718"
-          },
-      ]
+# data = [
+#           {
+#               "sex": "Male",
+#               "email": "pmarks1914@gmail.com",
+#               "balance": -960,
+#               "program": "DBS STATISTICS",
+#               "currency": "GHS",
+#               "fee_type": 1,
+#               "mobileNo": "0206358827",
+#               "accountNo": "01190001N",
+#               "full_name": "DADZIE DERRICK ",
+#               "last_name": "",
+#               "studentId": "01190001N",
+#               "first_name": "",
+#               "sessionName": "Morning",
+#               "admission_id": "01190001N",
+#               "currentLevel": "NON-HND I",
+#               "mobile_number": "0543418718",
+#               "recipient_address": "0543418718"
+#           },
+#           {
+#               "sex": "Male",
+#               "email": "zee@gmail.com",
+#               "balance": -960,
+#               "program": "DBS STATISTICS",
+#               "currency": "GHS",
+#               "fee_type": 1,
+#               "mobileNo": "0206358827",
+#               "accountNo": "01190001N",
+#               "full_name": "DADZIE DERRICK ",
+#               "last_name": "",
+#               "studentId": "01190001N",
+#               "first_name": "",
+#               "sessionName": "Morning",
+#               "admission_id": "01190001N",
+#               "currentLevel": "NON-HND I",
+#               "mobile_number": "0543418718",
+#               "recipient_address": "0543418718"
+#           },
+#           {
+#               "sex": "Male",
+#               "email": "zee@gmail.com",
+#               "balance": -960,
+#               "program": "DBS STATISTICS 000",
+#               "currency": "GHS",
+#               "fee_type": 1,
+#               "mobileNo": "0206358827",
+#               "accountNo": "01190001N",
+#               "full_name": "DADZIE DERRICK 000",
+#               "last_name": "",
+#               "studentId": "01190001N",
+#               "first_name": "",
+#               "sessionName": "Morning",
+#               "admission_id": "01190001N",
+#               "currentLevel": "NON-HND I",
+#               "mobile_number": "0543418718",
+#               "recipient_address": "0543418718"
+#           },
+#           {
+#               "sex": "Male",
+#               "email": "jay@gmail.com",
+#               "balance": -960,
+#               "program": "DBS STATISTICS",
+#               "currency": "GHS",
+#               "fee_type": 1,
+#               "mobileNo": "0206358827",
+#               "accountNo": "01190001N",
+#               "full_name": "DADZIE DERRICK ",
+#               "last_name": "",
+#               "studentId": "01190001N",
+#               "first_name": "",
+#               "sessionName": "Morning",
+#               "admission_id": "01190001N",
+#               "currentLevel": "NON-HND I",
+#               "mobile_number": "0543418718",
+#               "recipient_address": "0543418718"
+#           },
+#       ]
 # customers = []
 # for i in data:
 #   # print(i["email"])
@@ -225,6 +225,173 @@ data = [
 
 # print(l)
 
-a = dict((v['email'],v) for v in data).values()
+data = [
+    {
+        "id": "38b79d2d-3337-4fa6-9e1d-2a0db3ad4d4d",
+        "reference_id": "WP185583182676047",
+        "amount": "0.10",
+        "note": "test transaction",
+        "service": 6,
+        "status_code": "PENDING",
+        "source_metadata": {
+            "first_name": "Cornelius",
+            "last_name": "Ashley-Osuzoka",
+            "email": "user@gmail.com",
+            "mobile_number": "676064566",
+            "recipient_address": "Immueble CiSo, Boulevard de la liberte, Akwa Douala"
+        },
+        "device_info": {
+            "Content-Length": "428",
+            "Content-Type": "application/json",
+            "Authorization": "Api-Key d1AgnEwt.Ifca3nGHb4ZfsNpA2DCZ3UyqN8QjzJsz",
+            "User-Agent": "PostmanRuntime/7.29.0",
+            "Accept": "*/*",
+            "Postman-Token": "109b7d5b-f36e-44e5-8809-bc36235aa113",
+            "Host": "127.0.0.1:8007",
+            "Accept-Encoding": "gzip, deflate, br",
+            "Connection": "keep-alive"
+        },
+        "external_account_id": "0208556743",
+        "status_message": "Transaction is pending",
+        "created_at": "2022-08-03T14:54:46.378348Z"
+    },
+    {
+        "id": "9de7375f-93ee-47b4-9588-f201ca43249b",
+        "reference_id": "WP540534948824086",
+        "amount": "0.10",
+        "note": "test transaction voda",
+        "service": 6,
+        "status_code": "PENDING",
+        "source_metadata": {
+            "first_name": "Cornelius",
+            "last_name": "Ashley-Osuzoka",
+            "email": "user@gmail.com",
+            "mobile_number": "676064566",
+            "recipient_address": "Immueble CiSo, Boulevard de la liberte, Akwa Douala"
+        },
+        "device_info": {
+            "Content-Length": "433",
+            "Content-Type": "application/json",
+            "Authorization": "Api-Key d1AgnEwt.Ifca3nGHb4ZfsNpA2DCZ3UyqN8QjzJsz",
+            "User-Agent": "PostmanRuntime/7.29.0",
+            "Accept": "*/*",
+            "Postman-Token": "a5b88a02-070a-4525-9cce-c7c8aa0483d9",
+            "Host": "127.0.0.1:8007",
+            "Accept-Encoding": "gzip, deflate, br",
+            "Connection": "keep-alive"
+        },
+        "external_account_id": "0208556743",
+        "status_message": "Transaction is pending",
+        "created_at": "2022-07-28T02:16:09.505629Z"
+    },
+    {
+        "id": "5e309e57-6b01-492e-9b34-7db858edf15c",
+        "reference_id": "WP560618341540211",
+        "amount": "0.10",
+        "note": "test transaction",
+        "service": 6,
+        "status_code": "PENDING",
+        "source_metadata": {
+            "first_name": "Cornelius",
+            "last_name": "Ashley-Osuzoka",
+            "email": "user@gmail.com",
+            "mobile_number": "676064566",
+            "recipient_address": "Immueble CiSo, Boulevard de la liberte, Akwa Douala"
+        },
+        "device_info": {
+            "Content-Length": "402",
+            "Content-Type": "application/json",
+            "Authorization": "Api-Key d1AgnEwt.Ifca3nGHb4ZfsNpA2DCZ3UyqN8QjzJsz",
+            "User-Agent": "PostmanRuntime/7.29.0",
+            "Accept": "*/*",
+            "Postman-Token": "41b4a585-c274-4698-9a66-2f05282fbfdb",
+            "Host": "127.0.0.1:8007",
+            "Accept-Encoding": "gzip, deflate, br",
+            "Connection": "keep-alive"
+        },
+        "external_account_id": "0208556743",
+        "status_message": "Transaction is pending",
+        "created_at": "2022-07-27T10:14:00.324431Z"
+    },
+    {
+        "id": "e32cbb40-8b29-4f63-9bb0-47d0ea65e3f1",
+        "reference_id": "WP542999506574261",
+        "amount": "0.10",
+        "note": "test transaction",
+        "service": 6,
+        "status_code": "FAILED",
+        "source_metadata": {
+            "first_name": "Cornelius",
+            "last_name": "Ashley-Osuzoka",
+            "email": "user@gmail.com",
+            "mobile_number": "676064566",
+            "recipient_address": "Immueble CiSo, Boulevard de la liberte, Akwa Douala"
+        },
+        "device_info": {
+            "Content-Length": "402",
+            "Content-Type": "application/json",
+            "Authorization": "Api-Key d1AgnEwt.Ifca3nGHb4ZfsNpA2DCZ3UyqN8QjzJsz",
+            "User-Agent": "PostmanRuntime/7.29.0",
+            "Accept": "*/*",
+            "Postman-Token": "85bd9f61-afdf-4260-9d8c-278240999199",
+            "Host": "127.0.0.1:8007",
+            "Accept-Encoding": "gzip, deflate, br",
+            "Connection": "keep-alive"
+        },
+        "external_account_id": "0208556743",
+        "status_message": "Transaction is pending",
+        "created_at": "2022-07-27T10:12:47.928455Z"
+    },
+    {
+        "id": "f7839329-56d9-4b54-99ce-52e281d202ca",
+        "reference_id": "WP381776181478976",
+        "amount": "0.10",
+        "note": "test",
+        "service": 2,
+        "status_code": "SUCCESSFUL",
+        "source_metadata": None,
+        "device_info": None,
+        "external_account_id": "0558230944",
+        "status_message": "",
+        "created_at": "2022-07-26T14:22:41.120781Z"
+    },
+]
 
-print(a)
+
+# a = dict((v['external_account_id'],v) for v in data).values()
+
+# print(a)
+
+# del_key = 'source_metadata'
+# for items in data:
+#     if del_key in items:
+#         del items[del_key]
+# print(data)
+
+# del_key = 'source_metadata'
+# for items in data:
+#     if del_key in items:
+#         del items[del_key]
+# print(data)
+
+# for i in data:
+#   if i.source_metadata == None:
+#       i.surce_metadata = ""
+#   if i.device_info == None:
+#     i.device_info = ""
+
+# a = dict((v['external_account_id'],v) for v in data).values()
+
+# print(data.distinct())
+
+
+# Please use this link to submit additional 
+# information: https://grnh.se/r/4e1b0f7f5756a29b589d458aa64a11us.
+
+secret = "hhcjkdskja213nm,cne"
+
+msg = ""
+for char in secret:
+  msg += chr(ord(char) - 1)
+
+  print(msg)
