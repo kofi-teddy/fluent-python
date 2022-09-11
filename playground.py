f3b4ce3bb645c928ef942d1d02ba60b055e10999
kofi-teddy <agudogot@gmail.com>
2022-09-11 23:38:06 +0000

    ui updates

--- playground.py
+++ playground.py
@@ -388,10 +388,95 @@ data = [
 # Please use this link to submit additional 
 # information: https://grnh.se/r/4e1b0f7f5756a29b589d458aa64a11us.
 
-secret = "hhcjkdskja213nm,cne"
+# secret = "hhcjkdskja213nm,cne"
 
-msg = ""
-for char in secret:
-  msg += chr(ord(char) - 1)
+# msg = ""
+# for char in secret:
+#   msg += chr(ord(char) - 1)
 
-  print(msg)
+#   print(msg)
+
+
+
+
+# # Importing library
+# import qrcode
+ 
+# # Data to be encoded
+# data = 'QR Code using make() function'
+ 
+# # Encoding data using make() function
+# img = qrcode.make(data)
+ 
+# print(img)
+# # Saving as an image file
+# # img.save('MyQRCode1.png')
+
+
+
+
+# from cryptography.fernet import Fernet
+ 
+# # we will be encrypting the below string.
+# message = "hello geeks"
+ 
+# # generate a key for encryption and decryption
+# # You can use fernet to generate
+# # the key or use random key generator
+# # here I'm using fernet to generate key
+ 
+# key = Fernet.generate_key()
+ 
+# # Instance the Fernet class with the key
+ 
+# fernet = Fernet(key)
+ 
+# # then use the Fernet class instance
+# # to encrypt the string string must
+# # be encoded to byte string before encryption
+# encMessage = fernet.encrypt(message.encode())
+ 
+# print("original string: ", message)
+# print("encrypted string: ", encMessage)
+ 
+# # decrypt the encrypted string with the
+# # Fernet instance of the key,
+# # that was used for encrypting the string
+# # encoded byte string is returned by decrypt method,
+# # so decode it to string with decode methods
+# decMessage = fernet.decrypt(encMessage).decode()
+ 
+# print("decrypted string: ", decMessage)
+
+
+
+
+import rsa
+ 
+# generate public and private keys with
+# rsa.newkeys method,this method accepts
+# key length as its parameter
+# key length should be atleast 16
+publicKey, privateKey = rsa.newkeys(512)
+ 
+# this is the string that we will be encrypting
+message = "hello geeks"
+ 
+# rsa.encrypt method is used to encrypt
+# string with public key string should be
+# encode to byte string before encryption
+# with encode method
+encMessage = rsa.encrypt(message.encode(),
+                         publicKey)
+ 
+print("original string: ", message)
+print("encrypted string: ", encMessage)
+ 
+# the encrypted message can be decrypted
+# with ras.decrypt method and private key
+# decrypt method returns encoded byte string,
+# use decode method to convert it to string
+# public key cannot be used for decryption
+decMessage = rsa.decrypt(encMessage, privateKey).decode()
+ 
+print("decrypted string: ", decMessage)
