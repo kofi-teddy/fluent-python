aacd689fc8177189a47dab5de4f0f722e215b073
kofi-teddy <agudogot@gmail.com>
2024-05-25 22:14:56 +0000

    update: trie

--- trie.py
+++ trie.py
@@ -31,10 +31,32 @@ class Trie:
                 # If the current character isn't found among 
                 # the current node's children, we add
                 # the character as a new child node:
-                newNode = TrieNode() 
-                current_node.children[char] = newNode
+                new_node = TrieNode() 
+                current_node.children[char] = new_node
             # Follow this new node:
-            current_node = newNode
+            current_node = new_node
         # After inserting the entire word into the trie,
         # we add a * key at the end:
         current_node.children["*"] = None
+
+    def collect_all_words(self, node=None, word="", words=[]):
+        # This method accepts three arguments. The first is the
+        # node whose descendants we're collecting words from.
+        # The second argument, word, begins as an empty string,
+        # and we add characters to it as we move through the trie. # The third argument, words, begins as an empty array,
+        # and by the end of the function will contain all the words
+        # from the trie.
+        # The current node is the node passed in as the first parameter, # or the root node if none is provided:
+        current_node = node or self.root
+
+        # We iterate through all the current node's children:
+        for key, child_node in current_node.children.items():
+            # If the current key is *, it means we hit the end of a 
+            # complete word, so we can add it to our words array: 
+            if key == "*":
+                words.append(word)
+            else:
+                # If we're still in the middle of a word:
+                # We recursively call this function on the child node.
+                self.collect_all_words(child_node, word + key, words) 
+        return words
